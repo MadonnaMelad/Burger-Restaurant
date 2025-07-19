2207bce87d43f27accaef835a1a9f162aabd8ccd
MadonnaMelad <madonnamelad93@gmail.com>
2025-07-19 21:38:39 +0300

    Fixing Routers3

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import Home from './pages/Home';      
 import Cart from './pages/Cart';

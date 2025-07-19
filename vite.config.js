1c698c337d0e80a893a1d75e37a32c1b0e57aa18
MadonnaMelad <madonnamelad93@gmail.com>
2025-07-19 21:25:42 +0300

    Fixing Routers2

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// ✨ الحل هنا في تغيير الـ base:
+
 export default defineConfig({
   plugins: [react()],
-  base: '/Burger-Restaurant/' // اسم الريبو على GitHub
+  base: '/Burger-Restaurant/'  
 });
